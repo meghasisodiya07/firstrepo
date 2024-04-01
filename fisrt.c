94a2b4e6ddc9d83c5f832218b734b7b02924ce00
meghasisodiya07 <164147037+meghasisodiya07@users.noreply.github.com>
2024-04-01 16:17:33 +0530

    fisrt.c

---
+++ fisrt.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+void main()
+//sum of tow number
+/*{
+	int n1,n2,sum;
+	n1=5,
+	n2=9,
+	sum=n1+n2;
+	printf("sum of %d & %d = %d",n1,n2,sum);
+}*/
+
+//subtraction of tow number
+/*  {
+    float n1,n2,sub;
+    n1=1.4;
+    n2=5.1;
+    sub=n1-n2;
+    printf("substraction of %.1f & %1.f = %1.f",n1,n2,sub);
+  }*/
+  //multiplication of two number
+ /* {
+  	int n1,n2,mul;
+  	n1=6;
+  	n2=4;
+  	mul=n1*n2;
+  	printf("multiplication of %d & %d = %d",n1,n2,mul);
+  }*/
+  //divition of two number
+ /* {
+  	int n1,n2,div;
+  	n1=16;
+  	n2=2;
+  	div=n1/n2;
+  	printf("divition of %d & %d = %d",n1,n2,div);
+  }*/
+  //modulus of tow number
+ /* {
+  	int n1,n2,mod;
+  	n1=8;
+  	n2=3;
+  	mod=n1%n2;
+  	printf("modulus of %d & %d = %d",n1,n2,mod);
+  }*/
+  //swap any two no. using third variable
+/* {
+  	int x,y;
+  	x=10;
+  	y=40;
+  	printf("before swapping %d & %d",x,y);
+  	int z;
+  	z=x;
+  	x=y;
+  	y=z;
+  	printf("after swapping %d & %d",x,y);
+  }*/
+  //calculate simle interest
+ /* {
+  	int p,r,t,si;
+  	p=1000;
+  	r=2;
+  	t=2;
+  	si=(p*r*t)/100;
+  	printf("simple interest =%d",si);
+  }*/
+  //swap any two no. without using third variable
+ /* {
+  	int a,b;
+  	a=20;
+  	b=30;
+  	printf("before swaping %d & %d\n",a,b);
+  	a=a+b;
+  	b=a-b;
+  	a=a-b;
+  	printf("after swaping %d & %d",a,b);
+  }*/
+  // area of circle
+  
+ /*{
+  	float pi;
+  	pi=3.14;
+  	int r, area;
+  	r=5;
+  	area = pi*r*r;
+  	printf ("area of circle = %d",area);
+  }*/
+  //area of rectangle
+ /* {
+  	int area,l,w;
+  	l=7;
+  	w=4;
+  	area=l*w;
+  	printf("area of rectangle = %d",area);
+  }*/
+  //Area of tringle
+/* {
+  int area,b,h;
+  b=5;
+  h=7;
+  area=(b*h)/2;
+  printf("area of triangle = %d",area);
+}*/
+ //enter no. by user and print it's square & cube
+/* {
+ 	int num;
+ 	printf("enter a num: ");
+ 	scanf("%d",&num);
+ 	printf("square is : %d",num);
+    return 0;
+ }*/
+ 
